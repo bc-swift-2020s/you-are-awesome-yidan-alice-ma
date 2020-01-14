1c23385a6fbb64266f9a0f298b9dc30a4a51f8db
Yidan Ma <madt@bc.edu>
2020-01-14 13:12:31 -0500

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by mayidan on 1/14/20.
+//  Copyright © 2020 mayidan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
